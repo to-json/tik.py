901eeacaedaf0a7850e9a1623d4deecdc442d137
j <>
2021-04-11 21:00:27 -0500

    initial commit of tik.py

---
+++ README.md
@@ -0,0 +1,73 @@
+# Whassis?
+
+it's a goofy little front end to py-jira that i thought might help others
+
+basically, the library is great! and there are other perfectly good jira 
+cli options. but they are varying levels of difficult to use with broswer-cookie
+which, in turn, is basically required if you're stuck in certain corporate
+configs. and well, if you weren't stuck in a corp setting, you probably wouldn't
+be using jira.
+
+## Contributing?
+
+um...prs welcome i guess? but also, it's pretty trash and probably you 
+just shouldn't? 
+
+## Usage?
+so, `pip install -r requirements.txt`, using whatever pip installs stuff that's
+reachable by the `python` or `python3` on your `path`. then just stick tik.py
+somewhere on your path and `chmod +x` it. make sure `tik.ini` is in the same
+dir. a `tik.ini.example` is provided, you know the drill.
+
+## But I'm not on a unix!
+
+condolences
+
+## How do i make a fields section in my `tik.ini`?
+
+this is a special hell
+
+jira calls every displayed field that's not part of the unmodified jira config
+`customfield_<int>`. "How to i get a mapping from those to their names?" you ask?
+
+you don't!
+
+i sat in ipython for about an hour looking at random fields and setting the visible
+fields on my test ticket to different things, in order to pull out the 4 or so fields
+that are in my personal fields.ini. i could probably do some sort of nifty list
+comprehension to get every one of them that carries a name with it, but, turns out,
+most don't!
+
+jira; not even once.
+
+for that reason, ipython is in the requirements, and an 'embed' flag is provided. this
+is also how i personally do feature development, i just pop into the repl that way and
+play with ideas until they work.
+
+this is a good idea because the jira module has some broken bits (like attach_file*)
+so you really need to just...try everything you intend to include
+
+\* this is apparently fixed in 3.o, but 3.0 is not on pypi. i might vendor it at some point.
+
+if you do so, `jira` is the var that contains your jira instance
+
+## Does it just do the things that are in the help, provided by running `tik.py -h`?
+
+mostly! if you pipe stuff to `tik <ticket-id> -c` it will use that as the comment body,
+inside a code block, because that's a central usecase for me. i wanted to be able to pipe
+in attachments too, but, see above about `attach_file` being Just Broken.
+
+there also an interactive transition mode if you invoke `tik.py <ticket-id> -t` without a 
+transition after it, and ... iono honestly you should probably read the argument parsing
+stanza, i'm forgetful and almost forgot to write docs at all. in fact this file exists
+mostly because git repos look like shit on github if they don't have one.
+
+## Why not an ethical or viral license?
+
+no viral license because if you're using it at all, it's probably for work, because jira.
+no 'ethical' license because it's just not that big of a project. also, if
+there was a 'do no harm' clause you probably couldn't use it at work, becuase, there
+is no ethical (consumption|development) blah blah etc.
+
+but also, please do as little harm as possible? like, not as part of the license, just as
+one person asking another.
---
+++ tik.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+from jira import JIRA
+from jira import JIRAError
+import browser_cookie3
+import argparse
+import os
+import sys
+import configparser
+from IPython import embed
+
+# i'd rather these not be wierd globals but, then we'd have to pass jira
+# to everything. could do something with an object and initializer, but, no.
+
+config = configparser.ConfigParser()
+config.read('tik.ini')
+fields = config.items('fields')
+browser = config.get('other', 'browser')
+jira_url = config.get('other', 'jira_url')
+
+cookies = getattr(browser_cookie3, browser)()
+jira = JIRA({'cookies': cookies, 
+                'server': jira_url})
+
+# Buncha named functions because then i can read the dispatch code
+# Most of these did not need functions, but, idgaf
+
+def add_comment(ticket, comment):
+    jira.add_comment(ticket, comment)
+
+def display_comments(ticket):
+    print("Comments:")
+    for i, comment in enumerate(jira.comments(ticket)):
+        print(comment.author.displayName + ": " + comment.body)
+
+def transition(ticket, state):
+    jira.transition_issue(ticket, state)
+
+# This function is a mess
+def interactive_transition(ticket):
+    transitions = [(i, t['id'], t['name']) for i, t in enumerate(jira.transitions(ticket))]
+    for t in transitions:
+        print(str(t[0]) + ": " + str(t[2]))
+
+    target = None
+    while not (target and target.isdigit()):
+        target = input("? ")
+
+    try:
+        transition(ticket, transitions[int(target)][1])
+    except JIRAError as e:
+        print(e.text)
+        print("Unable to transition to that state, open ticket in a browser(y/n)?")
+        answer = None
+        while not (answer and (answer == 'y' or answer == 'n')):
+            answer = input("? ")
+        if answer == 'y':
+            open_ticket(ticket)
+        else:
+            exit()
+
+# This shit...
+# So jira has these fields whose names are not attached to them, and instead
+# they are called customfield_<integer>, and not all of them apply to every
+# ticket. This function does awfulness iterating over a list of fields from 
+# the config to print the fields if they exist. It's a seperate function for 
+# my repl-testing convenience
+
+def display_extended_fields(ticket, fields):
+    for field in fields:
+        field_accessor = "customfield_" + field[0]
+        if hasattr(ticket.fields, field_accessor):
+            print("{0}: {1}".format(field[1], 
+                                    getattr(ticket.fields, field_accessor)))
+
+def assignee(ticket):
+    if ticket.fields.assignee:
+        return ticket.fields.assignee.displayName
+    else:
+        return 'None'
+
+def display_ticket_full(ticket, fields):
+    '''Shows a ticket with it's comments.''' # Separated because Reasons
+    print(ticket.key + ": " + ticket.fields.summary)
+    print("Assignee: {0} --- Status: {1} --- Type: {2}"
+            .format(assignee(ticket),
+                    ticket.fields.status.name,
+                    ticket.fields.issuetype.name))
+    print("Description:\n" + ticket.fields.description)
+    print()
+    display_extended_fields(ticket, fields)
+    print()
+    display_comments(ticket)
+
+def open_ticket(ticket):
+    os.execl('/usr/bin/open', 'open', ticket.permalink())
+
+def main():
+    # Argparse fun
+    parser = argparse.ArgumentParser()
+    parser.add_argument("ticket", help="ticket to interact with", type=str)
+    parser.add_argument("-c", "--comment", help="comment on a ticket", 
+                        nargs="?", const=False)
+    parser.add_argument("-f", "--file", 
+                        help="load a file in order write as a comment")
+    parser.add_argument("-t", "--transition", help="transition a ticket", 
+                        nargs="?", const=False)
+    parser.add_argument("-d", "--display", help="display a ticket",
+                        action='store_true')
+    parser.add_argument("-o", "--open", help="transition a ticket",
+                        action='store_true')
+    parser.add_argument("-e", "--embed", help="load an ipython console",
+                        action='store_true')
+    args = parser.parse_args()
+
+    ticket = jira.issue(args.ticket)
+    # This all depends on some kinda absurd punning where, by explicitly
+    # setting a given arg to 'False' instead of it's default 'None', i can
+    # dispatch on the kind of non-thing that arg is, coupled with some poorly
+    # documented bullshit in argparse about how there are actually 2 'defaults'
+    # for every arg, the 'default' and the 'const', where the const is used if
+    # the flag is supplied but an argument is not passed to the flag
+
+    action_taken = None
+    if args.comment:
+        add_comment(ticket, args.comment)
+        display_comments(ticket)
+        action_taken = 1
+    elif args.comment == False:
+        if args.file:
+            with open(args.file, 'r') as file:
+                data = file.read()
+                add_comment(ticket, data)
+        elif not sys.stdin.isatty():
+            data = "{code}" + sys.stdin.read() + "{code}"
+            add_comment(ticket, data)
+        else:
+            display_comments(ticket)
+        action_taken = 1
+    elif args.transition:
+        transition(ticket, args.transition)
+        action_taken = 1
+    elif args.transition == False:
+        interactive_transition(ticket)
+        action_taken = 1
+    elif args.embed:
+        embed()
+
+
+    if args.display:
+        if action_taken:
+            ticket = jira.issue(args.ticket)
+        display_ticket_full(ticket, fields)
+    if args.open:
+        open_ticket(ticket)
+
+if __name__ == "__main__":
+    main()
